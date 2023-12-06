6a794990d40acd46ef0a730d623ed58fa5bc0e7a
kanmanikarthiga <karthiga.selva98@gmail.com>
2023-12-06 22:52:51 +0530

    first commit

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const { route } = require("./Routes/CategoryRoutes");
 const PORT = 5005;
 const app = express();
-// const cors = require("cors")
+const cors = require("cors")
 
-// app.use(cors({
-//     origin:"http://localhost:5005"
-// }))
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+   });
 app.use("/api", route)
 app.listen(PORT, () =>{
     try{
